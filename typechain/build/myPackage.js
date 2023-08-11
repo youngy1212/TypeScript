7788c14cbdbe77069d056a8ec66b1c501bd8c6f3
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-08-11 19:52:16 +0900

    DefinitelyTyped

---
+++ typechain/build/myPackage.js
@@ -0,0 +1,21 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) {
+    return true;
+}
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns {number}
+ */
+export function exit(code) {
+    return code + 1;
+}
+//ts-check ts에게 이 js도 확인하라는 뜻
+//ts가 코멘을 읽고 확인! JS자체는 오류나지 않음
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,8 +1,29 @@
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
 
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
+//타입스크립트로 작성되지 않은 패키지를 import할때
+//crypto ts가 모르겠다는 오류 -> d파일 작성 (비효율적)
+// DefinitelyTyped  => TypeScript type 정의를 위한 리포지토리
+// 해당 패키지를 다운
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha234").update(toHash).digest("hex");
   }
 }
